10ea94407f46f14b24d0bde2d0d50e86cc85e8da
rezitadhina <68811466+rezita11@users.noreply.github.com>
2023-03-16 15:08:55 +0700

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = "secretkey";
+
+auth = (req, res, next) => {
+    let header = req.headers.authorization;
+    let token = header && header.split(' ')[1];
+
+    let jwtHeader = {
+        algoritm: "HS256"
+    };
+
+    if(token == null) return res.sendStatus(401).json({message: "Unauthorized"});
+
+    jwt.verify(token, SECRET_KEY, jwtHeader, (err, usr) =>{
+        if (err) return res.sendStatus(401).json({message : "Invalid Token"});
+
+        req.user = usr;
+        console.log(req.user);
+        next();
+    });
+
+};
+
+module.exports = auth;
